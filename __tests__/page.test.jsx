02bf9e30dcc3c635c383d3d7300515208fc3a04a
Ewelina Wierzbicka <ewierzbicka@provenexpert.com>
2024-03-02 17:02:13 +0100

    add jest

---
+++ __tests__/page.test.jsx
@@ -0,0 +1,13 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Register from "../app/register/page";
+
+describe("Register", () => {
+  it("renders a heading", () => {
+    render(<Register />);
+
+    const heading = screen.getByRole("heading", { level: 3 });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
